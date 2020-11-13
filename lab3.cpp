aaaa99e6bca195fc2dae306e664d4de5bfdea464
piotrmikula <p.mikula14@wp.pl>
2020-11-13 13:04:47 +0000

    Zadanie13

--- lab3.cpp
+++ lab3.cpp
@@ -4,7 +4,7 @@ class Figura
 {
 public:
     Figura(double pole_in) : pole(pole_in) {}
-    ~Figura() {}
+    virtual ~Figura() {}
 
     double       getPole() const { return pole; }
     virtual void id() const { std::cout << "Figura o polu rownym " << pole << std::endl; }
@@ -17,7 +17,7 @@ class Kolo : public Figura
 {
 public:
     Kolo(double r_in) : Figura{3.1415 * r_in * r_in} {}
-    ~Kolo() {}
+    virtual ~Kolo() { std::cout << "Kolo zostalo zniszczone\n"; }
 
     void id() const override { std::cout << "Kolo o polu rownym " << getPole() << std::endl; }
 
@@ -29,7 +29,7 @@ class Kwadrat : public Figura
 {
 public:
     Kwadrat(double a_in) : Figura{a_in * a_in} {}
-    ~Kwadrat() {}
+    virtual ~Kwadrat() { std::cout << "Kwadrat zostal zniszczony\n"; }
 
     void id() const override { std::cout << "Kwadrat o polu rownym " << getPole() << std::endl; }
 
@@ -46,7 +46,11 @@ int main()
 {
     Figura* f = new Kwadrat{4};
     id(*f);
+    delete f;
+
     Figura* f2 = new Kolo{3};
     id(*f2);
+    delete f2;
+
     puts("Ostatnia linijka w kodzie");
 }
