8671ff39c79655dc8de31a1adf25cdfcf4309d19
piotrmikula <p.mikula14@wp.pl>
2020-11-13 09:49:26 +0000

    Zadanie2

--- lab3.cpp
+++ lab3.cpp
@@ -3,11 +3,17 @@
 class Figura
 {
 public:
+    void   setPole(double p) { pole = p; }
+    double getPole() { return pole; }
+
 private:
     double pole;
 };
 
 int main()
 {
+    Figura f;
+    f.setPole(5);
+    std::cout << "Pole figury wynosi " << f.getPole() << std::endl;
     puts("Ostatnia linijka w kodzie");
 }
