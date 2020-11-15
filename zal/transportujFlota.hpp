655ed67b3fa47e6c6d0e2881d56646db4895adde
piotrmikula <p.mikula14@wp.pl>
2020-11-15 01:51:30 +0000

    zejsciowka

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int licznikTowaru = 0;                           //licznik towaru na poczatku
+    unsigned int liczbaZaglowcow = 0;                         //licznik zaglowcow na poczatku
+
+    if(towar == 0)                                            //jesli liczba towaru jest rowna 0
+      return liczbaZaglowcow;
+
+    Stocznia stocznia{};                                      
+
+    while(licznikTowaru < towar)
+    {
+      Statek* statek = stocznia();
+      licznikTowaru += (statek->transportuj());              //dodanie towaru znajdującego sie na statku
+
+      if(dynamic_cast<Zaglowiec*>(statek))                   //sprawdzenie czy statek jest zaglowcem
+        liczbaZaglowcow += 1;
+
+      delete statek;                                         //niszczenie statku
+    }
+
+   return liczbaZaglowcow; 
 }
